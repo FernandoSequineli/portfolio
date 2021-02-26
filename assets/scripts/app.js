3940904ff0cdb425e1ddb8ffc3b65dfabc9c0e8b
FernandoSequineli <sequineli.fernando@gmail.com>
2021-02-26 10:52:29 +0000

    update

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -57,7 +57,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am an Enthusiastic Computing Student", "like working with new Tools/Technologies.", "like problem solving.", "like coding.", "am constantly envolving!"
+				"am an Enthusiastic Computing Student", "like working with new Tools/Technologies.", "like problem solving.", "like coding.", "am constantly evolving!"
 			],
 			stringsElement: null,
 			// typing speed
