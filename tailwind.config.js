25d88e4bea6a348a11289f99f4b2232a80d2744a
Fernando Sequineli <sequineli.fernando@gmail.com>
2022-10-05 18:12:23 +0100

    added about, portfolio, experience and contact pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -7,5 +6,17 @@ module.exports = {
       signature: ["Great Vibes"],
     },
   },
+  height: (theme) => ({
+    auto: "auto",
+    ...theme("spacing"),
+    full: "100%",
+    screen: "calc(var(--vh) * 100)",
+  }),
+  minHeight: (theme) => ({
+    0: "0",
+    ...theme("spacing"),
+    full: "100%",
+    screen: "calc(var(--vh) * 100)",
+  }),
   plugins: [],
 };
