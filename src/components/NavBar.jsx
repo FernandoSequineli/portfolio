048168ab5b6563a582e5f6ebfba3d99295897f24
Fernando Sequineli <sequineli.fernando@gmail.com>
2022-10-06 14:50:16 +0100

    css nav fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,7 +36,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <h1 className="text-5xl font-signature ml-2">Fernando Sequineli</h1>
+        <h1 className="text-5xl font-signature ml-2">Fernando</h1>
       </div>
 
       <ul className="hidden md:flex">
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -37,7 +37,7 @@ const Portfolio = () => {
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen pb-10"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
@@ -55,11 +55,11 @@ const Portfolio = () => {
                 alt=""
                 className="rounded-md duration-200 hover:scale-105"
               />
-              <div className="flex items-center justify-center -mt-10">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+              <div className="flex items-center justify-center -mt-3">
+                <button className="w-1/2 px-6 py-2 m-4 duration-200 hover:scale-105">
                   Demo
                 </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button className="w-1/2 px-6 py-2 m-4 duration-200 hover:scale-105">
                   Code
                 </button>
               </div>
